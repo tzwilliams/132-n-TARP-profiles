00349e42e7267171322a99b164d36dfca49eb262
Taylor Williams <will1630@purdue.edu>
2018-11-30 17:34:45 -0500

    first functioning version

--- R/building feature vectors.R
+++ R/building feature vectors.R
@@ -28,7 +28,8 @@
 #
 # Changelog:
 #   2018.10.23.   Init code
-#   2018.11.27.   
+#   2018.11.27.   continued init development
+#   2018.11.30.   First complete working version
 #                   
 # Feature wishlist:  (*: planned but not complete)
 #     
@@ -50,7 +51,7 @@ require(tidyverse)
 require(readxl)
 
 
-## Read data ####
+######### Read Data ##########
 # _import the LO mapping data ####
 LO_mapping <- read_xlsx(#path = file.choose(), 
                         path = "C:\\Users\\Taylor Williams\\OneDrive\\Documents\\_Purdue (OD)\\__Milestones\\2_Prelim (OD)\\132 data + info\\ENGR132_Sp18_assessment_plan (update1).xlsx",
@@ -75,8 +76,12 @@ for (i in 1:nrow(LO_mapping)) {
 
 
 # _import BlackBoard LO data ####
-data_raw_assessment <- read_csv(file.choose())
+data_raw_assessment <- read_csv(#file = file.choose(),
+                                file = "C:\\Users\\Taylor Williams\\OneDrive\\Documents\\_Purdue (OD)\\__Milestones\\2_Prelim (OD)\\132 data + info\\132 deID data (sp18)\\_132 deID data. complete. clean.csv")
 
+
+
+######### Clean Data ##########
 # extract the numeric LO IDs
 LO_ID <- str_match(string = data_raw_assessment$`Rubric Row`, pattern = "\\d{2}\\.\\d{2}")[,1]
 
@@ -93,58 +98,19 @@ LOs_unused <- anti_join(LOs_in_map, LOs_in_assessment)
 
 
 
+# get list of student IDs
+student_ids_all <- unique(df_subset$"User ID")
+
+
+
+########## SUBSETS for initial development and testing #########
 # filter on the 01.xx LOs
 LO_ID_range <- str_detect(string = df$LO_ID, pattern = "^01\\.\\d{2}")
 df_subset <- df[LO_ID_range, ]
-LO_subset <- unique(df_subset$LO_ID)
+LO_subset <- sort(unique(df_subset$LO_ID))
 
-
-# get list of student IDs
-student_ids <- unique(df$"User ID")
-
-
-# build LO level feature vector
-proficiency_levels <- tbble(value = c(4,3,2,1,0),
-                            label = c("4_Proficient", "3_Developing", 
-                                      "2_Emerging", "1_Insufficient Evidence", 
-                                      "0_No Attempt"), 
-                            rubric_column =  c("Proficient", "Developing", 
-                                               "Emerging", "Insufficient Evidence", 
-                                               "No Attempt"), 
-                            rubric_column_alt =  c("Proficient", "Developing", 
-                                                   "Emerging", "Insufficient Evidence", 
-                                                   "Did Not Attempt"))
-stu_LO_FV <- tibble(student_id = student_ids)
-FV_names <- character()
-for (LO in LO_subset) {
-  FV_names <- c(FV_names, paste0(LO, " - ", proficiency_levels$label))
-}
-
-###### CHECKED TO HERE #######
-# Tue Nov 27 18:48:04 2018 ------------------------------
-##############################
-
-# for each student subset the LOs and calculate average LO proficiency (TW Note: for now I'm ignoring the point value and only looking at the proficiency)
-for (cur_stu in student_ids) {
-  cur_stu_LOs <- df_subset[df_subset$User.ID == cur_stu, ]
-
-  for (LO in LO_subset){
-    cur_assessment <- cur_stu_LOs[cur_stu_LOs$LO_ID == LO, "Rubric.Column"]
-    LO_value_sum <- 0
-    for (i in 1:length(cur_assessment)){
-      level <- cur_assessment[i]
-      # LO_value_sum <- LO_value_sum +
-        # proficiency_levels[proficiency_levels$rubric_column == level, "value"]
-      # proficiency_levels[proficiency_levels$rubric_column == level, "value"]
-
-      # stu_LO_FV[cur_stu, FV_names == paste0(LO, " - ", level)]
-    }
-    LO_avg <- LO_value_sum/i
-
-
-  }
-
-}
+# only include first 100 students
+student_ids <- student_ids_all[1:100]
 
 
 
@@ -153,35 +119,71 @@ for (cur_stu in student_ids) {
 #   assessment score for a particular LO: prob(assessment score|LO) 
 #   (e.g., if they earn "Proficient" to 2 of 3 items for LO 01.03 
 #   then 2/3 will be recorded)
+#   
+## build LO level feature vectors (FV)
+# construct column names for feature vectors
+proficiency_levels <- tibble(value = c(4,3,2,1,0),
+                             label = c("4_Proficient", "3_Developing", 
+                                       "2_Emerging", "1_Insufficient Evidence", 
+                                       "0_No Attempt"), 
+                             rubric_column =  c("Proficient", "Developing", 
+                                                "Emerging", "Insufficient Evidence", 
+                                                "No Attempt"), 
+                             rubric_column_alt =  c("Proficient", "Developing", 
+                                                    "Emerging", "Insufficient Evidence", 
+                                                    "Did Not Attempt"))
 
-#Initialize the matrix with a column of the user IDs in the dataset
-probMatrixRaw <- df %>% distinct(User.ID)
-
-
-#creating columns for the probMatrixRaw.  Initializing all probabilities to 0
-for (FV_field in FV_names) {
-  probMatrixRaw <- tibble::add_column(.data = probMatrixRaw, 
-                                      !!(FV_field) := 0)
+# make list of FV name
+FV_names <- character()
+for (LO in LO_subset) {
+  FV_names <- c(FV_names, paste0(LO, " - ", proficiency_levels$label))
 }
 
 
+# construct student FV table
+stu_LO_FV <- tibble(student_id = student_ids)
 
-#find the sum total of LOs assosciated with each LO code
-#construct dataframe to hold totals, one row per code found in coded_Qs, initalize count to zero
-codeTotalQs <- data.frame("Code"=levels(coded_Qs$Code1), "Total"=0)
-
-for(i in 1:nlevels(coded_Qs$Code1)){
-  #store the string for the current code 
-  codeTotalQs$Total[i] <- sum(coded_Qs$Code1==levels(coded_Qs$Code1)[i])
+# append feature vector names to the main table (init. values to 0)
+for (i in 1:length(FV_names)) {
+  stu_LO_FV <- stu_LO_FV %>% add_column(!!FV_names[i] := 0)
+  
 }
+stu_LO_FV <- column_to_rownames(stu_LO_FV, var = "student_id")
+  
 
 
 
 
-# 
-# 
-# str_match(string = names(stu_LO_FV)[2:ncol(stu_LO_FV)], 
-#           pattern = "(?<=_).*")
+#For each student subset the LOs and calculate average LO proficiency 
+#   (TW Note: for now I'm ignoring the point value and working with the proficiency level)
+for (cur_stu in student_ids) {
+  cur_stu_LOs <- df_subset[df_subset$"User ID" == cur_stu, ]
+
+  for (LO in LO_subset){
+    cur_assessments <- cur_stu_LOs[cur_stu_LOs$LO_ID == LO, "Rubric Column"]
+    # LO_value_sum <- 0.0
+    
+    if(nrow(cur_assessments) > 0){
+      # loop through the recorded proficencies for the current LO
+      for (i in 1:length(cur_assessments$`Rubric Column`)){
+        level <- cur_assessments$`Rubric Column`[[i]]
+        
+        # LO_value_sum <- LO_value_sum +
+        #   proficiency_levels[proficiency_levels$rubric_column == level, "value"]
+        
+        cur_colName <- paste0(LO, " - ",
+                              proficiency_levels$label[proficiency_levels$rubric_column == level])
+        
+        # increment the count for this LO's profiency level
+        stu_LO_FV[cur_stu, cur_colName] <- stu_LO_FV[cur_stu, cur_colName] +1
+      }
+    
+      
+      # calc probability for these LOs
+      stu_LO_FV[cur_stu, str_detect(string = FV_names, pattern = paste0("^", LO))] <- 
+        stu_LO_FV[cur_stu, str_detect(string = FV_names, pattern = paste0("^", LO))]/i
+    } # end IF
+  } # end LO FOR
+} # end stu FOR
 
 
-# build Category feature vector
